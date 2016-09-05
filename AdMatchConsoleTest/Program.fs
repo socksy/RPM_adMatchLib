133770a10a208ea3ea51dbba6ed036f0230e7982
sudipto80 <sudipto80@yahoo.com>
2016-09-06 01:04:33 +0530

    First version of RPM_adMatchLib
    To match advertisements as per channel constraints

---
+++ AdMatchConsoleTest/Program.fs
@@ -0,0 +1,68 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open System
+open RPM.adMatching
+
+[<EntryPoint>]
+let main argv = 
+    //Sample Channels 
+    let Channels = [
+                    {
+                        ID="1"; 
+                        Type = Blog;  
+                        URL="abc.com"; 
+                        Language="de"; 
+                        Country="Germany";
+                        UserInterests = ["Fashion";"Automobile"];
+                        Constraints = [
+                                            ("Language",["German"]);
+                                            ("PreferenceOrder",["Fashion";"Beauty";"Automobile"])
+                                      ]
+                     };
+                     {
+                        ID="2"; 
+                        Type = Blog;  
+                        URL="abc.com"; 
+                        Language="en"; 
+                        Country="UK";
+                        UserInterests = ["Automobile";"Cars";"Transportation";"Fashion"];
+                        Constraints = [
+                                            ("Language",["English"]);
+                                            ("PreferenceOrder",["Fashion";"Cars";"Automobile"])
+                                      ]
+                     }]
+                
+    //Sample advertisement
+    let ads =  [
+                {
+                    ID="1";
+                    StartDate=DateTime.Today.AddDays(-30.0);
+                    EndDate=DateTime.Today.AddDays(30.0);
+                    Language="German";
+                    Type=["Beauty";"Fashion"];
+                    Views = 1
+                 };
+                {
+                    ID="2";
+                    StartDate=DateTime.Today.AddDays(-30.0);
+                    EndDate=DateTime.Today.AddDays(30.0);
+                    Language="Italian";
+                    Type=["Beauty";"Fashion"];
+                    Views = 2
+                };
+                {
+                    ID="3";
+                    StartDate=DateTime.Today.AddDays(-40.0);
+                    EndDate=DateTime.Today.AddDays(10.0);
+                    Language="German";
+                    Type=["Beauty";"Fashion"];
+                    Views = 2
+                }
+              ]
+
+    //First channel is from Germany and preferred language is "German"
+    //And that of the 
+    printfn "%A" (matchAdv Channels.[0] ads)//should print either 1 or 3
+    printfn "%A" (matchAdv Channels.[1] ads)//should print "None"
+    0
+    
---
+++ Library1/matcher.fs
@@ -0,0 +1,96 @@
+﻿
+
+module RPM.adMatching
+
+open System
+open System.Linq
+
+type ChannelType = Blog 
+                 | Site 
+
+type Constraint = (string * string list)  
+
+let maxViews = 5000
+
+type Channel = {
+                    ID : string;
+                    Type: ChannelType; 
+                    URL : string ; 
+                    Language : string; 
+                    Country : string ; 
+                    UserInterests : string list;
+                    Constraints : Constraint list 
+               }
+
+type Advertisement = {
+                        ID : string;
+                        StartDate : DateTime; 
+                        EndDate : DateTime; 
+                        Language : string; //Language
+                        Type : string list;//What is the ad about 
+                        mutable Views : int 
+                     }
+
+
+
+let dropExpiredAds (ads:Advertisement list) =
+     ads |> List.filter (fun ad -> DateTime.Today.CompareTo(ad.EndDate) < 0)
+
+//This is a global constraint
+let spareOldAds(ads:Advertisement list)=
+    ads |> List.filter (fun ad -> ad.Views < maxViews)
+
+
+let checkForLanguage (ad:Advertisement)(values:string list)=
+    values.Contains ad.Language
+
+let checkForPreferenceOrder(ad:Advertisement)(values:string list)=
+   //This is now a vanilla matching of string containing 
+   //This can be improved by some other sophisticated matching like
+   //Jaccard Index
+    ad.Type |> List.filter(fun t -> values.Contains t)|>List.length > 0
+    
+let getConstraintChecker (tag:string)=
+    match tag with 
+     | "Language" -> checkForLanguage
+     | "PreferenceOrder" -> checkForPreferenceOrder
+     
+    
+let getCommon (ids : (string list) list) =
+    ids |> Seq.map (fun fs -> fs |> Set.ofList) 
+        |> Set.intersectMany 
+ 
+let incrementView (ad:Advertisement) =
+    ad.Views <- ad.Views + 1
+
+//Matches ads with the request channel 
+let matchAdv (request:Channel) (ads: Advertisement list) =
+    //Get rid of globally expired ads by "EndDate"
+    //And ads that have been viewed more than a set threshold
+    let liveAds = ads |> dropExpiredAds 
+                      |> spareOldAds  
+    //Find checkers (predicates) to be applied on advertisements
+    let constraintCheckers = request.Constraints 
+                                   |> List.map(fun constraints -> getConstraintChecker (fst constraints))
+    //Find values for constraints. 
+    let constraintValues = request.Constraints |> List.map snd 
+    //Combining these two 
+    let constraintValuesAndCheckers = List.zip constraintValues constraintCheckers 
+    //This contains the IDs of all the ads for which at least one of the predicate
+    //(Constraint function) returns true
+    let filtered = constraintValuesAndCheckers 
+                    |> List.map(fun valueAndChecker -> 
+                                  liveAds 
+                                       |> List.filter (fun ad -> (snd valueAndChecker) ad (fst valueAndChecker)))
+                    |> List.map (fun filteredAds -> filteredAds |> List.map (fun ad -> ad.ID))   
+    //Finds only the intersection of all such ids 
+    //resulting in a seq of ids of only the matching advertisements
+    let ids = filtered |> getCommon 
+
+    //Pick a random ad in case there are many.
+    //if not, it will pick the only matching one.
+    let adID = if (ids|>Seq.length) >0 then (ids |> Seq.toList).[(new Random()).Next(ids.Count  - 1)] 
+                            else "None"
+    if adID <> "None" then incrementView (ads.First(fun t -> t.ID = adID))
+    adID
+    
